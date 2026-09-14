6a1c288e5cde877ba739c192bc2adfde788dc491
leitingzz <3479309040@qq.com>
2026-09-14 21:39:58 +0800

    更新

---
+++ practices1.java
@@ -0,0 +1,14 @@
+public class practices1 {
+    public static void main(String[] args){
+        String name = "张三";
+        int age = 20;
+        double gpa = 3.8;
+        System.out.println("我叫" + name + ",今年" + age + ",GPA是" + gpa);
+        int a = 7;
+        int b = 2;
+        System.out.println("两数之和为：" + (a + b));
+        System.out.println("两数之差为：" + (a - b));
+        System.out.println("两数之积为：" + (a * b));
+        System.out.println("两数之商为：" + ((double)a / b));
+    }
+}
---
+++ practices3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class practices3 {
+    public static void main(String[] args){
+        int ans = 50;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你的猜测：");
+        int guess = scanner.nextInt();
+        if(guess > ans){
+            System.out.println("猜大了！");
+        }else if(guess < ans){
+            System.out.println("猜小了！");
+        }else{
+            System.out.println("猜对了！");
+        }
+        scanner.close();
+    }
+}
